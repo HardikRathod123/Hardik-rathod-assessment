0459cbf7ae84ea4d19e6385460e2d7c7773777b9
Hardik Rathod <hardik.r@rafltlabs.com>
2024-09-28 18:17:09 +0530

    feat: Adding AI button in linkedin comment form

--- entrypoints/content/index.tsx
+++ entrypoints/content/index.tsx
@@ -1,6 +1,27 @@
+import ReactDOM from 'react-dom/client';
+import LinkedInScript from './linkedin-script.tsx';
+import "../../assets/main.css"
+
 export default defineContentScript({
-    matches: ['https://www.linkedin.com/*'],
-    main() {
-        console.log('LinkedIn AI Reply extension loaded successfully!')
+    matches: ['*://*/*'],
+    cssInjectionMode: 'ui',
+    async main(ctx) {
+        const ui = await createShadowRootUi(ctx, {
+            name: 'language-learning-content-box',
+            position: 'inline',
+            onMount: (container) => {
+                console.log(container);
+                const root = ReactDOM.createRoot(container);
+                root.render(
+                        <LinkedInScript/>
+                );
+                return root;
+            },
+            onRemove: (root) => {
+                root?.unmount();
+            },
+        });
+
+        ui.mount();
     },
-  })
+});
